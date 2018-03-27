3b8a52b0539946dce6724bcdb05f940be9b8a0fd
wjanjua <37092405+wjanjua@users.noreply.github.com>
2018-03-27 00:51:59 -0500

    Changes
    Changes

--- js/Assignment5.js
+++ js/Assignment5.js
@@ -44,7 +44,7 @@ function Generatecustomerlist()
         
     }
     
-    objRequest.open("GET", url, true);
+    objRequest.open("GET",url,true);
     objRequest.send();
     
 }
@@ -54,9 +54,9 @@ function GenerateOutput(result)
     var count = 0;
     var displaytext = "";
     
-    for (count = 0; count < result.GetAllCustomersResult.length; count++)
+    for (count = 0; count <result.GetAllCustomersResult.length; count++)
     {
-        displaytext += result.GetAllCustomersResult[count].CustomerName + "," + result.GetAllCustomersResult[count].CustomerID + "," + result.GetAllCustomersResult[count].City + "<br>";   
+        displaytext += result.GetAllCustomersResult[count].CustomerName + "," + result.GetAllCustomersResult[count].CustomerId + "," + result.GetAllCustomersResult[count].City + "<br>";   
     }
     
     document.getElementById("customerlist").innerHTML = displaytext;
@@ -66,70 +66,70 @@ function GenerateOutput(result)
 function CustomerOrderHistory()
 {
     var objRequest = new XMLHttpRequest();
-    var url = "https://student.business.uab.edu/jsonwebservice/service1.svc/getCustomerOrderHistory/";
-    url += document.getElementById("custid").value;
+    var urltwo = "https://student.business.uab.edu/jsonwebservice/service1.svc/getCustomerOrderHistory/";
+    urltwo += document.getElementById("custid").value;
     
     objRequest.onreadystatechange = function()
     {
         if (objRequest.readyState == 4 && objRequest.status == 200)
         {
             var output = JSON.parse(objRequest.responseText);
-            GenerateOutput2(output);
+            GenerateOutputtwo(output);
         }
         
     }
     
-    objRequest.open("GET", url, true);
+    objRequest.open("GET", url2, true);
     objRequest.send();
     
 }
 
-function GenerateOutput2(result)
+function GenerateOutputtwo(result)
 {
     var count = 0;
-    var displaytext = "";
+    var displaytexttwo = "";
     
     for (count = 0; count < result.GetCustomerOrderHistory.length; count++)
     {
-        displaytext += result.GetCustomerOrderHistory[count].ProductNames + "," + result.GetCustomerOrderHistory[count].Quantities + "<br>";
+        displaytexttwo += result.GetCustomerOrderHistory[count].ProductNames + "," + result.GetCustomerOrderHistory[count].QuantitiesOrdered + "<br>";
         
     }
     
-    document.getElementById("orderhistory").innerHTML = displaytext;
+    document.getElementById("orderhistory").innerHTML = displaytexttwo;
 }
 
 //section 3
 function OrdersPlaced()
 {
     var objRequest = new XMLHttpRequest();
-    var url = "https://student.business.uab.edu/jsonwebservice/service1.svc/getOrdersForCustomer/";
-    url += document.getElementById("custid").value;
+    var urlthree = "https://student.business.uab.edu/jsonwebservice/service1.svc/getOrdersForCustomer/";
+    urlthree += document.getElementById("custid").value;
     
     objRequest.onreadystatechange = function()
     {
         if (objRequest.readyState == 4 && objRequest.status == 200)
         {
             var output = JSON.parse(objRequest.responseText);
-            GenerateOutput3(output);
+            GenerateOutputthree(output);
         }
         
     }
     
-    objRequest.open("GET", url, true);
+    objRequest.open("GET", urlthree, true);
     objRequest.send();
     
 }
 
-function GenerateOutput3(result)
+function GenerateOutputthree(result)
 {
     var count = 0;
-    var displaytext = "";
+    var displaytextthree = "";
     
     for (count = 0; count < result.GetOrdersForCustomer.length; count++)
     {
-        displaytext += result.GetOrdersForCustomer[count].OrderDate + "," + result.GetOrdersForCustomer[count].OrderID + "," + result.GetOrdersForCustomer[count].ShipAddress + "," + result.GetOrdersForCustomer.ShipCity + "," + result.GetOrdersForCustomer.ShipName + "," + result.GetOrdersForCustomer.ShipPostCode + "," + result.GetOrdersForCustomer.ShippedDate + "<br>";
+        displaytextthree += result.GetOrdersForCustomer[count].OrderDate + "," + result.GetOrdersForCustomer[count].OrderID + "," + result.GetOrdersForCustomer[count].ShipAddress + "," + result.GetOrdersForCustomer.ShipCity + "," + result.GetOrdersForCustomer.ShipName + "," + result.GetOrdersForCustomer.ShipPostCode + "," + result.GetOrdersForCustomer.ShippedDate + "<br>";
         
     }
     
-    document.getElementById("placeorders").innerHTML = displaytext;
+    document.getElementById("placeorders").innerHTML = displaytextthree;
 }
